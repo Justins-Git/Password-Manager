b887c9d5185c0065f24c43e602b443d768df59de
Justin <justintalbot1016@gmail.com>
2024-06-19 16:42:42 -0400

    Added barebone master account functionality

--- src/PasswordManager.py
+++ src/PasswordManager.py
@@ -15,33 +15,45 @@ class PasswordManager:
 
     # Creates an unique key to you if there is not one already and opens it
     def load_key(self):
+        # If the key file exists, open it and read the key
         if os.path.exists(self.key_file):
             with open(self.key_file, 'rb') as file:
                 key = file.read()
+        # If the key file does not exist, create a new key and save it
         else:
             key = Fernet.generate_key()
             with open(self.key_file, 'wb') as file:
                 file.write(key)
         return key
-
+    
+    # Loads the passwords from the data file
     def load_passwords(self):
+        # If the data file exists, open it and read the encrypted data
         if os.path.exists(self.data_file):
             with open(self.data_file, 'rb') as file:
                 encrypted_data = file.read()
             decrypted_data = self.cipher.decrypt(encrypted_data)
+            # Return the decrypted data
             return json.loads(decrypted_data)
         else:
             return {}
-
+        
+    # Saves the passwords to the data file
     def save_passwords(self):
         encrypted_data = self.cipher.encrypt(json.dumps(self.passwords).encode())
         with open(self.data_file, 'wb') as file:
             file.write(encrypted_data)
-
+            
+    # Adds a password to the password manager
     def add_password(self, site, username, password):
         self.passwords[site] = {'username': username, 'password': password}
         self.save_passwords()
-
+        
+    # Removes a password from the password manager
     def get_password(self, site):
+        # If the site exists in the passwords, copy the password to the clipboard
         pyperclip.copy(self.passwords.get(site, None)['password'])
         return self.passwords.get(site, None)
+    
+    
+        
